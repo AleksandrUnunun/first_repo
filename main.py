bcb91d39aee1f051c95f74258ce583cf28f5cbcc
Admin <Serdykow39@gmail.com>
2023-09-14 04:13:45 +0200

    0.5

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import random
+import string
 
 
 def rand_list():
     return [random.randint(0, 50) for i in range(30)]
 
+def rand_pass():
+    return "".join([random.choice(string.ascii_letters + string.punctuation) for _ in range(8)])
 
 a = rand_list()
-print(rand_list())
+print(rand_pass())
 
 
